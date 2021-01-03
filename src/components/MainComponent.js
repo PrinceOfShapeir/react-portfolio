90890b41a444f26d67e620c6cbe6fd162d675060
Sean Farley <somethingfarley@gmail.com>
2021-01-02 20:47:15 -0500

    set up home routes

---
+++ src/components/MainComponent.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Home from './HomeComponent';
+class Main extends Component {
+    componentDidMount() {
+        //do something
+    }
+
+    render() {
+            return (
+                <>
+                    {/*<Head />*/}
+                    <Home />
+                </>
+                    
+            );
+    }
+}
+
+export default Main;
